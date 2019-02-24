018608e89187a6a20980262e20538e4779caf5f3
Kenrick Hatton <kenrick.hatton@gmail.com>
2019-02-24 13:22:49 +0000

    removal of requrments file with typo

--- blog/settings.py
+++ blog/settings.py
@@ -81,7 +81,6 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
 if "DATABASE_URL" in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
@@ -94,7 +93,6 @@ else:
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 
 # Password validation
@@ -139,4 +137,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
